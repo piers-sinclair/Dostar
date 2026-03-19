5cc1728b66b7c5a17106acef375de4f76498cefd
Piers Sinclair <piers2307@gmail.com>
2026-03-19 09:32:38 +0000

    fix claude

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -5,4 +5,5 @@ az bicep install
 dotnet tool install -g dotnet-ef || true
 dotnet restore
 sudo chown vscode:vscode frontend/node_modules
+sudo chown -R vscode:vscode /home/vscode/.claude
 cd frontend && pnpm install
